91d4cb3e979702860934a5f82275c2b453f5a20e
junping.shao <junping.shao@capstonegaming.com>
2018-06-06 10:57:51 +0800

    修改，增加拖入材质球，在原有材质球基础上增加animMap，增加readMe,增加文档，增加路径拖入功能

--- Assets/AnimMapBaker/Script/AnimMapBaker.cs
+++ Assets/AnimMapBaker/Script/AnimMapBaker.cs
@@ -1,233 +1,230 @@
 ﻿/*
- * Created by jiadong chen
- * http://www.chenjd.me
- * 
  * 用来烘焙动作贴图。烘焙对象使用animation组件，并且在导入时设置Rig为Legacy
  */
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-/// <summary>
-/// 保存需要烘焙的动画的相关数据
-/// </summary>
-public struct AnimData
+namespace AnimMapBaker
 {
-    #region 字段
-
-    public int vertexCount;
-    public int mapWidth;
-    public List<AnimationState> animClips;
-    public string name;
-
-    private  Animation animation;
-    private SkinnedMeshRenderer skin;
-
-    #endregion
-
-    public AnimData(Animation anim, SkinnedMeshRenderer smr, string goName)
+    /// <summary>
+    /// 保存需要烘焙的动画的相关数据
+    /// </summary>
+    public struct AnimData
     {
-        vertexCount = smr.sharedMesh.vertexCount;
-        mapWidth = Mathf.NextPowerOfTwo(vertexCount);
-        animClips = new List<AnimationState>(anim.Cast<AnimationState>());
-        animation = anim;
-        skin = smr;
-        name = goName;
-    }
+        #region 字段
 
-    #region 方法
+        public int vertexCount;
+        public int mapWidth;
+        public List<AnimationState> animClips;
+        public string name;
 
-    public void AnimationPlay(string animName)
-    {
-        this.animation.Play(animName);
-    }
+        private  Animation animation;
+        private SkinnedMeshRenderer skin;
 
-    public void SampleAnimAndBakeMesh(ref Mesh m)
-    {
-        this.SampleAnim();
-        this.BakeMesh(ref m);
-    }
+        #endregion
 
-    private void SampleAnim()
-    {
-        if (this.animation == null)
+        public AnimData(Animation anim, SkinnedMeshRenderer smr, string goName)
         {
-            Debug.LogError("animation is null!!");
-            return;
+            vertexCount = smr.sharedMesh.vertexCount;
+            mapWidth = Mathf.NextPowerOfTwo(vertexCount);
+            animClips = new List<AnimationState>(anim.Cast<AnimationState>());
+            animation = anim;
+            skin = smr;
+            name = goName;
         }
 
-        this.animation.Sample();
-    }
+        #region 方法
 
-    private void BakeMesh(ref Mesh m)
-    {
-        if (this.skin == null)
+        public void AnimationPlay(string animName)
         {
-            Debug.LogError("skin is null!!");
-            return;
+            this.animation.Play(animName);
         }
 
-        this.skin.BakeMesh(m);
-    }
-
-
-    #endregion
-
-}
-
-/// <summary>
-/// 烘焙后的数据
-/// </summary>
-public struct BakedData
-{
-    #region 字段
-
-    public string name;
-    public float animLen;
-    public byte[] rawAnimMap;
-    public int animMapWidth;
-    public int animMapHeight;
-    public Vector3 minPos;
-    public Vector3 maxPos;
-
-    #endregion
-
-    public BakedData(string name, float animLen, Texture2D animMap, Vector3 minPos, Vector3 maxPos)
-    {
-        this.name = name;
-        this.animLen = animLen;
-        this.animMapHeight = animMap.height;
-        this.animMapWidth = animMap.width;
-        this.rawAnimMap = animMap.GetRawTextureData();
-        this.maxPos = maxPos;
-        this.minPos = minPos;
-    }
-}
-
-/// <summary>
-/// 烘焙器
-/// </summary>
-public class AnimMapBaker{
-
-    #region 字段
-
-    private AnimData? animData = null;
-    private List<Vector3> vertices = new List<Vector3>();
-    private Mesh bakedMesh;
-
-    private List<BakedData> bakedDataList = new List<BakedData>();
-
-    #endregion
-
-    #region 方法
-
-    public void SetAnimData(GameObject go)
-    {
-        if(go == null)
+        public void SampleAnimAndBakeMesh(ref Mesh m)
         {
-            Debug.LogError("go is null!!");
-            return;
+            this.SampleAnim();
+            this.BakeMesh(ref m);
         }
 
-        Animation anim = go.GetComponent<Animation>();
-        SkinnedMeshRenderer smr = go.GetComponentInChildren<SkinnedMeshRenderer>();
-
-        if(anim == null || smr == null)
+        private void SampleAnim()
         {
-            Debug.LogError("anim or smr is null!!");
-            return;
+            if (this.animation == null)
+            {
+                Debug.LogError("animation is null!!");
+                return;
+            }
+
+            this.animation.Sample();
         }
-        this.bakedMesh = new Mesh();
-        this.animData = new AnimData(anim, smr, go.name);
+
+        private void BakeMesh(ref Mesh m)
+        {
+            if (this.skin == null)
+            {
+                Debug.LogError("skin is null!!");
+                return;
+            }
+
+            this.skin.BakeMesh(m);
+        }
+        #endregion
     }
 
-    public List<BakedData> Bake()
+    /// <summary>
+    /// 烘焙后的数据
+    /// </summary>
+    public struct BakedData
     {
-        if(this.animData == null)
+        #region 字段
+
+        public string name;
+        public float animLen;
+        public byte[] rawAnimMap;
+        public int animMapWidth;
+        public int animMapHeight;
+        public Vector3 minPos;
+        public Vector3 maxPos;
+
+        #endregion
+
+        public BakedData(string name, float animLen, Texture2D animMap, Vector3 minPos, Vector3 maxPos)
         {
-            Debug.LogError("bake data is null!!");
+            this.name = name;
+            this.animLen = animLen;
+            this.animMapHeight = animMap.height;
+            this.animMapWidth = animMap.width;
+            this.rawAnimMap = animMap.GetRawTextureData();
+            this.maxPos = maxPos;
+            this.minPos = minPos;
+        }
+    }
+
+    /// <summary>
+    /// 烘焙器
+    /// </summary>
+    public class AnimMapBaker{
+
+        #region 字段
+
+        private AnimData? animData = null;
+        private List<Vector3> vertices = new List<Vector3>();
+        private Mesh bakedMesh;
+
+        private List<BakedData> bakedDataList = new List<BakedData>();
+
+        #endregion
+
+        #region 方法
+
+        public void SetAnimData(GameObject go)
+        {
+            if(go == null)
+            {
+                Debug.LogError("go is null!!");
+                return;
+            }
+
+            Animation anim = go.GetComponent<Animation>();
+            SkinnedMeshRenderer smr = go.GetComponentInChildren<SkinnedMeshRenderer>();
+
+            if(anim == null || smr == null)
+            {
+                Debug.LogError("anim or smr is null!!");
+                return;
+            }
+            this.bakedMesh = new Mesh();
+            this.animData = new AnimData(anim, smr, go.name);
+        }
+
+        public List<BakedData> Bake()
+        {
+            if(this.animData == null)
+            {
+                Debug.LogError("bake data is null!!");
+                return this.bakedDataList;
+            }
+
+            //每一个动作都生成一个动作图
+            for(int i = 0; i < this.animData.Value.animClips.Count; i++)
+            {
+                if(!this.animData.Value.animClips[i].clip.legacy)
+                {
+                    Debug.LogError(string.Format("{0} is not legacy!!", this.animData.Value.animClips[i].clip.name));
+                    continue;
+                }
+                BakePerAnimClip(this.animData.Value.animClips[i]);
+            }
+
             return this.bakedDataList;
         }
 
-        //每一个动作都生成一个动作图
-        for(int i = 0; i < this.animData.Value.animClips.Count; i++)
+        private void BakePerAnimClip(AnimationState curAnim)
         {
-            if(!this.animData.Value.animClips[i].clip.legacy)
+            int curClipFrame = 0;
+            float sampleTime = 0;
+            float perFrameTime = 0;
+
+            //获取总帧数（帧率乘以秒数） 转换成2的幂
+            curClipFrame = Mathf.ClosestPowerOfTwo((int)(curAnim.clip.frameRate * curAnim.length));
+            //总秒数/总帧数 获得每帧的时间（s）
+            perFrameTime = curAnim.length / curClipFrame;
+
+            //mapWidth是顶点数 大的最小的2的幂
+            Texture2D animMap = new Texture2D(this.animData.Value.mapWidth, curClipFrame, TextureFormat.RGB24, false);
+            animMap.name = string.Format("{0}_{1}.animMap", this.animData.Value.name, curAnim.name);
+            this.animData.Value.AnimationPlay(curAnim.name);
+            Vector3 minPos = new Vector3(0, 0, 0);
+            Vector3 maxPos = new Vector3(0, 0, 0);
+            //所有帧，所有坐标，最大最小值
+            for(int i = 0; i < curClipFrame; i++)
             {
-                Debug.LogError(string.Format("{0} is not legacy!!", this.animData.Value.animClips[i].clip.name));
-                continue;
+                curAnim.time = sampleTime;
+                this.animData.Value.SampleAnimAndBakeMesh(ref this.bakedMesh);
+                for (int j = 0; j < this.bakedMesh.vertexCount; j++)
+                {
+                    Vector3 vertex = this.bakedMesh.vertices[j];
+                    if (vertex.x > maxPos.x)
+                        maxPos.x = vertex.x;
+                    else if (vertex.x < minPos.x)
+                        minPos.x = vertex.x;
+
+                    if (vertex.y > maxPos.y)
+                        maxPos.y = vertex.y;
+                    else if (vertex.y < minPos.y)
+                        minPos.y = vertex.y;
+
+                    if (vertex.z > maxPos.z)
+                        maxPos.z = vertex.z;
+                    else if (vertex.z < minPos.z)
+                        minPos.z = vertex.z;
+                }
+                sampleTime += perFrameTime;
             }
+            sampleTime = 0;
+            for (int i = 0; i < curClipFrame; i++)
+            {
+                curAnim.time = sampleTime;
 
-            BakePerAnimClip(this.animData.Value.animClips[i]);
+                this.animData.Value.SampleAnimAndBakeMesh(ref this.bakedMesh);
+                //x轴，顶点，y轴帧
+                for (int j = 0; j < this.bakedMesh.vertexCount; j++)
+                {
+                    Vector3 vertex = this.bakedMesh.vertices[j];
+                    Vector3 diff = new Vector3(0.0f, 0.0f, 0.0f);
+                    diff.x = (vertex.x - minPos.x) / (maxPos.x - minPos.x);
+                    diff.y = (vertex.y - minPos.y) / (maxPos.y - minPos.y);
+                    diff.z = (vertex.z - minPos.z) / (maxPos.z - minPos.z);
+                    animMap.SetPixel(j, i, new Color(diff.x, diff.y, diff.z));
+                }
+                sampleTime += perFrameTime;
+            }
+            animMap.Apply();
+
+            this.bakedDataList.Add(new BakedData(animMap.name, curAnim.clip.length, animMap, minPos, maxPos));
         }
-
-        return this.bakedDataList;
+        #endregion
     }
-
-    private void BakePerAnimClip(AnimationState curAnim)
-    {
-        int curClipFrame = 0;
-        float sampleTime = 0;
-        float perFrameTime = 0;
-
-        //获取总帧数（帧率乘以秒数） 转换成2的幂
-        curClipFrame = Mathf.ClosestPowerOfTwo((int)(curAnim.clip.frameRate * curAnim.length));
-        //总秒数/总帧数 获得每帧的时间（s）
-        perFrameTime = curAnim.length / curClipFrame;
-
-        //mapWidth是顶点数 大的最小的2的幂
-        Texture2D animMap = new Texture2D(this.animData.Value.mapWidth, curClipFrame, TextureFormat.RGBAHalf, false);
-
-        animMap.name = string.Format("{0}_{1}.animMap", this.animData.Value.name, curAnim.name);
-        this.animData.Value.AnimationPlay(curAnim.name);
-        Vector3 minPos = new Vector3(0, 0, 0);
-        Vector3 maxPos = new Vector3(0, 0, 0);
-        //所有帧，所有坐标，最大最小值
-        for(int i = 0; i < curClipFrame; i++)
-        {
-            curAnim.time = sampleTime;
-            this.animData.Value.SampleAnimAndBakeMesh(ref this.bakedMesh);
-            for (int j = 0; j < this.bakedMesh.vertexCount; j++)
-            {
-                Vector3 vertex = this.bakedMesh.vertices[j];
-                if (vertex.x > maxPos.x)
-                    maxPos.x = vertex.x;
-                else if (vertex.x < minPos.x)
-                    minPos.x = vertex.x;
-
-                if (vertex.y > maxPos.y)
-                    maxPos.y = vertex.y;
-                else if (vertex.y < minPos.y)
-                    minPos.y = vertex.y;
-
-                if (vertex.z > maxPos.z)
-                    maxPos.z = vertex.z;
-                else if (vertex.z < minPos.z)
-                    minPos.z = vertex.z;
-            }
-            sampleTime += perFrameTime;
-        }
-        sampleTime = 0;
-        for (int i = 0; i < curClipFrame; i++)
-        {
-            curAnim.time = sampleTime;
-
-            this.animData.Value.SampleAnimAndBakeMesh(ref this.bakedMesh);
-            //x轴，顶点，y轴帧
-            for (int j = 0; j < this.bakedMesh.vertexCount; j++)
-            {
-                Vector3 vertex = this.bakedMesh.vertices[j];
-                Vector3 diff = new Vector3(0.0f, 0.0f, 0.0f);
-                diff.x = (vertex.x - minPos.x) / (maxPos.x - minPos.x);
-                diff.y = (vertex.y - minPos.y) / (maxPos.y - minPos.y);
-                diff.z = (vertex.z - minPos.z) / (maxPos.z - minPos.z);
-                animMap.SetPixel(j, i, new Color(diff.x, diff.y, diff.z));
-            }
-            sampleTime += perFrameTime;
-        }
-        animMap.Apply();
-
-        this.bakedDataList.Add(new BakedData(animMap.name, curAnim.clip.length, animMap, minPos, maxPos));
-    }
-    #endregion
+        
 }
+
--- Assets/AnimMapBaker/Script/Editor/AnimMapBakerWindow.cs
+++ Assets/AnimMapBaker/Script/Editor/AnimMapBakerWindow.cs
@@ -1,171 +1,178 @@
-﻿/*
- * Created by jiadong chen
- * http://www.chenjd.me
- */
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-public class AnimMapBakerWindow : EditorWindow {
-
-    private enum SaveStrategy
+namespace AnimMapBaker
+{
+    public class AnimMapBakerWindow : EditorWindow
     {
-        AnimMap,//only anim map
-        Mat,//with shader
-        Prefab//prefab with mat
-    }
-
-    #region 字段
-
-    public static GameObject targetGo;
-    private static AnimMapBaker baker;
-    private static string path = "DefaultPath";
-    private static string subPath = "SubPath";
-    private static SaveStrategy stratege = SaveStrategy.AnimMap;
-    private static Shader animMapShader;
-
-    #endregion
-
-
-    #region  方法
-
-    [MenuItem("Window/AnimMapBaker")]
-    public static void ShowWindow()
-    {
-        EditorWindow.GetWindow(typeof(AnimMapBakerWindow));
-        baker = new AnimMapBaker();
-        animMapShader = Shader.Find("chenjd/AnimMapShader");
-    }
-
-    void OnGUI()
-    {
-        targetGo = (GameObject)EditorGUILayout.ObjectField(targetGo, typeof(GameObject), true);
-        subPath = targetGo == null ? subPath : targetGo.name;
-        EditorGUILayout.LabelField(string.Format("保存路径output path:{0}", Path.Combine(path, subPath)));
-        path = EditorGUILayout.TextField(path);
-        subPath = EditorGUILayout.TextField(subPath);
-
-        stratege = (SaveStrategy)EditorGUILayout.EnumPopup("保存策略output type:", stratege);
-
-
-        if (GUILayout.Button("Bake"))
+        private enum SaveStrategy
         {
-            if(targetGo == null)
+            AnimMap,      //only anim map
+            Mat,          //with mat
+            Prefab        //prefab with mat
+        }
+
+        #region 字段
+
+        public static GameObject targetGo;
+        public static Material targetMat;
+        private static AnimMapBaker baker;
+        private static string path = "CapstonesRes/Game/Models/Scene/Flag";
+        private static string subPath = "";
+        private static SaveStrategy stratege = SaveStrategy.Prefab;
+        private static Shader animMapShader;
+
+        #endregion
+
+        #region  方法
+
+        [MenuItem("3DArt/AnimMapBaker")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(AnimMapBakerWindow));
+            baker = new AnimMapBaker();
+            animMapShader = Shader.Find("capstone/AnimMapShader");
+        }
+
+        void OnGUI()
+        {
+            targetGo = (GameObject)EditorGUILayout.ObjectField(targetGo, typeof(GameObject), true);
+            subPath = targetGo == null ? subPath : targetGo.name;
+            EditorGUILayout.LabelField(string.Format("保存路径output path:{0}", Path.Combine(path, subPath)));
+            //获得一个长300的框  
+            Rect rect = EditorGUILayout.GetControlRect(GUILayout.Width(400));
+            //将上面的框作为文本输入框  
+            path = EditorGUI.TextField(rect, path);
+            subPath = EditorGUILayout.TextField(subPath);
+
+            //如果鼠标正在拖拽中或拖拽结束时，并且鼠标所在位置在文本输入框内  
+            if ((Event.current.type == EventType.DragUpdated
+              || Event.current.type == EventType.DragExited)
+              && rect.Contains(Event.current.mousePosition))
             {
-                EditorUtility.DisplayDialog("err", "targetGo is null！", "OK");
-                return;
-            }
-
-            if(baker == null)
-            {
-                baker = new AnimMapBaker();
-            }
-
-            baker.SetAnimData(targetGo);
-
-            List<BakedData> list = baker.Bake();
-
-            if(list != null)
-            {
-                for(int i = 0; i < list.Count; i++)
+                //改变鼠标的外表  
+                DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
                 {
-                    BakedData data = list[i];
-                    Save(ref data);
+                    path = DragAndDrop.paths[0];
+                }
+            }
+
+            stratege = (SaveStrategy)EditorGUILayout.EnumPopup("保存策略output type:", stratege);
+            if(stratege == SaveStrategy.Prefab || stratege == SaveStrategy.Mat)
+            {
+                targetMat = (Material)EditorGUILayout.ObjectField(targetMat, typeof(Material), true);
+            }
+
+            if (GUILayout.Button("Bake"))
+            {
+                if (targetGo == null)
+                {
+                    EditorUtility.DisplayDialog("err", "targetGo is null, 请填第一个空！", "OK");
+                    return;
+                }
+
+                if (baker == null)
+                {
+                    baker = new AnimMapBaker();
+                }
+
+                baker.SetAnimData(targetGo);
+
+                List<BakedData> list = baker.Bake();
+
+                if (list != null)
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        BakedData data = list[i];
+                        Save(ref data);
+                    }
                 }
             }
         }
-    }
 
-
-    private void Save(ref BakedData data)
-    {
-        switch(stratege)
+        private void Save(ref BakedData data)
         {
-            case SaveStrategy.AnimMap:
-                SaveAsAsset(ref data);
-                break;
-            case SaveStrategy.Mat:
-                SaveAsMat(ref data);
-                break;
-            case SaveStrategy.Prefab:
-                SaveAsPrefab(ref data);
-                break;
-        }
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-    }
-
-    private Texture2D SaveAsAsset(ref BakedData data)
-    {
-        string folderPath = CreateFolder();
-        Texture2D animMap = new Texture2D(data.animMapWidth, data.animMapHeight, TextureFormat.RGBAHalf, false);
-
-        animMap.LoadRawTextureData(data.rawAnimMap);
-        AssetDatabase.CreateAsset(animMap, Path.Combine(folderPath, data.name + ".asset"));
-        return animMap;
-    }
-
-    private Material SaveAsMat(ref BakedData data)
-    {
-        if(animMapShader == null)
-        {
-            EditorUtility.DisplayDialog("err", "shader is null!!", "OK");
-            return null;
+            switch (stratege)
+            {
+                case SaveStrategy.AnimMap:
+                    SaveAsAsset(ref data);
+                    break;
+                case SaveStrategy.Mat:
+                    SaveAsMat(ref data);
+                    break;
+                case SaveStrategy.Prefab:
+                    SaveAsPrefab(ref data);
+                    break;
+            }
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
 
-        if(targetGo == null || !targetGo.GetComponentInChildren<SkinnedMeshRenderer>())
+        private Texture2D SaveAsAsset(ref BakedData data)
         {
-            EditorUtility.DisplayDialog("err", "SkinnedMeshRender is null!!", "OK");
-            return null;
+            string folderPath = CreateFolder();
+            Texture2D animMap = new Texture2D(data.animMapWidth, data.animMapHeight, TextureFormat.RGB24, false);
+
+            animMap.LoadRawTextureData(data.rawAnimMap);
+            AssetDatabase.CreateAsset(animMap, Path.Combine(folderPath, data.name + ".asset"));
+            return animMap;
         }
 
-        SkinnedMeshRenderer smr = targetGo.GetComponentInChildren<SkinnedMeshRenderer>();
-        Material mat = new Material(animMapShader);
-        Texture2D animMap = SaveAsAsset(ref data);
-        mat.SetTexture("_MainTex", smr.sharedMaterial.mainTexture);
-        mat.SetTexture("_AnimMap", animMap);
-        mat.SetFloat("_AnimLen", data.animLen);
-        Vector4 minPos = new Vector4(data.minPos.x, data.minPos.y, data.minPos.z, 0.0f);
-        Vector4 maxPos = new Vector4(data.maxPos.x, data.maxPos.y, data.maxPos.z, 0.0f);
-        mat.SetVector("_MinPos", minPos);
-        mat.SetVector("_MaxPos", maxPos);
-
-        string folderPath = CreateFolder();
-        AssetDatabase.CreateAsset(mat, Path.Combine(folderPath, data.name + ".mat"));
-
-        return mat;
-    }
-
-    private void SaveAsPrefab(ref BakedData data)
-    {
-        Material mat = SaveAsMat(ref data);
-
-        if(mat == null)
+        private Material SaveAsMat(ref BakedData data)
         {
-            EditorUtility.DisplayDialog("err", "mat is null!!", "OK");
-            return;
+            if (targetGo == null || !targetGo.GetComponentInChildren<SkinnedMeshRenderer>())
+            {
+                EditorUtility.DisplayDialog("err", "SkinnedMeshRender is null!!", "OK");
+                return null;
+            }
+
+            SkinnedMeshRenderer smr = targetGo.GetComponentInChildren<SkinnedMeshRenderer>();
+            Texture2D animMap = SaveAsAsset(ref data);
+            Material mat = new Material(targetMat);
+            mat.SetTexture("_AnimMap", animMap);
+            mat.SetFloat("_AnimLen", data.animLen);
+            Vector4 minPos = new Vector4(data.minPos.x, data.minPos.y, data.minPos.z, 0.0f);
+            Vector4 maxPos = new Vector4(data.maxPos.x, data.maxPos.y, data.maxPos.z, 0.0f);
+            mat.SetVector("_MinPos", minPos);
+            mat.SetVector("_MaxPos", maxPos);
+
+            string folderPath = CreateFolder();
+            AssetDatabase.CreateAsset(mat, Path.Combine(folderPath, data.name + ".mat"));
+
+            return mat;
         }
 
-        GameObject go = new GameObject();
-        go.AddComponent<MeshRenderer>().sharedMaterial = mat;
-        go.AddComponent<MeshFilter>().sharedMesh = targetGo.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
-        string folderPath = CreateFolder();
-        PrefabUtility.CreatePrefab(Path.Combine(folderPath, data.name + ".prefab").Replace("\\", "/"), go);
-    }
-
-    private string CreateFolder()
-    {
-        string folderPath = Path.Combine("Assets/" + path,  subPath);
-        if (!AssetDatabase.IsValidFolder(folderPath))
+        private void SaveAsPrefab(ref BakedData data)
         {
-            AssetDatabase.CreateFolder("Assets/" + path, subPath);
+            Material mat = SaveAsMat(ref data);
+
+            if (mat == null)
+            {
+                EditorUtility.DisplayDialog("err", "mat is null!!", "OK");
+                return;
+            }
+
+            GameObject go = new GameObject();
+            go.AddComponent<MeshRenderer>().sharedMaterial = mat;
+            go.AddComponent<MeshFilter>().sharedMesh = targetGo.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+            string folderPath = CreateFolder();
+            PrefabUtility.CreatePrefab(Path.Combine(folderPath, data.name + ".prefab").Replace("\\", "/"), go);
         }
-        return folderPath;
+
+        private string CreateFolder()
+        {
+            string folderPath = Path.Combine(path, subPath);
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                AssetDatabase.CreateFolder(path, subPath);
+            }
+            return folderPath;
+        }
+
+        #endregion
     }
-
-    #endregion
-
-
 }
